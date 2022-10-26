9748e75630256316dcebc073f0cca67365f21580
Paul Chibueze <94766465+chibuezedev@users.noreply.github.com>
2022-10-26 13:46:00 +0100

    Update ageController.js

--- controller/ageController.js
+++ controller/ageController.js
@@ -3,35 +3,36 @@ const { format } = require("timeago.js");
 
 exports.getAge = async (dateString, res, req, next) => {
 
-  try {
+    try {
 
-        //calculate the for the age
+        const dob = req.query.dob;
+        
+        if (dob === undefined || dob === ""){
+            res.status(400).send("Please add a valid parameter")
+        }
+         
+        let finalDob = dob.split("-")
+
+
+        if (new Date(finalDob).toString() === "Invalid date"){
+            let error = new Error("Invalid date format")
+            throw error;
+        }
+
         const today = new Date();
         const birthDate = new Date(dateString);
         const month = today.getMonth() - birthDate.getMonth();
         const age = today.getFullYear() - birthDate.getFullYear();
       
-        //validate all neccessary inputs
         if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
             age--;
         }
-          return age;
-
-      if (age === undefined || age === ""){
-        res.status(400).send("Please add a valid parameter")
-    }
-     
-    if (new Date(age).toString() === "Invalid date"){
-        let error = new Error("Invalid date format")
-        throw error;
-    }
-     //format input to standard date format
-
-      res.status(200).send(age)
+      const timestamp = format(finalDob)
+      return res.status(200).send({age: age, timestamp: timestamp})
     } catch (err) {
        next (err)
     }
-    }
+}
   
 
 
